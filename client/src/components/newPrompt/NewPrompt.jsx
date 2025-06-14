de793826ea552c87351b159a642d89339f5bdb5e
Bhavya Vaishnav <vaishnavbhavya1701@gmail.com>
2025-06-14 22:32:13 +0530

    Removed backend/node_modules from tracking

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -1,8 +1,15 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./newPrompt.css";
 import Upload from "../upload/upload";
+import { IKContext, IKUpload, IKImage } from "imagekitio-react";
+
 
 const NewPrompt = () => {
+  const [img, setImg] = useState({
+    isLoading: false,
+    error: "",
+    dbData: {},
+  });
   const endRef = useRef(null);
 
   useEffect(() => {
@@ -10,10 +17,21 @@ const NewPrompt = () => {
   }, []);
 
   return (
-    <>  
+    <>
+      {/* ADD NEW CHAT */}
+      {img.isLoading && <div className="">Loading...</div>}
+      {img.dbData && typeof img.dbData === "object" && img.dbData.filePath && (
+        <IKImage
+          urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+          path={img.dbData.filePath}
+          width={380}
+          transformation={[{width:380}]}
+        />
+      )}
+
       <div className="endChat" ref={endRef}></div>
       <form className="newForm">
-        <Upload/>
+        <Upload setImg={setImg} />
         <input type="file" id="file" multiple={false} hidden />
         <input type="text" placeholder="Ask anything..." />
         <button>
